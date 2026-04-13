3cdc44d8e1b06c327db8dcad11b77e94cba4c971
wjmcool8 <6661004144@telegram.org>
2026-04-14 07:19:11 +0800

    Fix: Change single quotes to double quotes for string literals in app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile('proguard-android-optimize.txt'),
+                getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
