09f409edd70b7f3a4bbc021188a011cacfa0befe
wjmcool8 <wjmcool8@gmail.com>
2026-04-14 16:44:23 +0800

    修复插件配置：添加apply false

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "7.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
 
 android {
